dd4079320abd3bcf5e207b454032dc4e3e4de918
BesGj <72765141+BesGj@users.noreply.github.com>
2020-10-26 14:59:54 +0100

    Initial commit

---
+++ src/FormSuccess.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FormSuccess = () => {
+return (
+ <div className='form-content-right'>
+   <div className='form-success'> We received your request!</div>
+<img src='img/dog.jpg' alt='success-image' className='form-img-2' />
+</div>
+)
+}
+
+export default FormSuccess;
